5cee2b1fb7b8c16a5383be7b8d7406b01c1fd2c8
EmuTheEmo <jaysgames.jsg@gmail.com>
2022-06-15 18:16:31 +0100

    Added Emerald and Obsidian Armor

--- src/main/java/com/jay/moretools/item/ModItems.java
+++ src/main/java/com/jay/moretools/item/ModItems.java
@@ -28,6 +28,34 @@ public class ModItems {
             () -> new ArmorItem(ModArmorMaterials.COPPER, EquipmentSlot.FEET,
                     new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
 
+    //Emerald Armor
+    public static final RegistryObject<Item> EMERALD_HELMET = ITEMS.register("emerald_helmet",
+            () -> new ArmorItem(ModArmorMaterials.EMERALD, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> EMERALD_CHESTPLATE = ITEMS.register("emerald_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.EMERALD, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> EMERALD_LEGGINGS = ITEMS.register("emerald_leggings",
+            () -> new ArmorItem(ModArmorMaterials.EMERALD, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> EMERALD_BOOTS = ITEMS.register("emerald_boots",
+            () -> new ArmorItem(ModArmorMaterials.EMERALD, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+
+    //Obsidian Armor
+    public static final RegistryObject<Item> OBSIDIAN_HELMET = ITEMS.register("obsidian_helmet",
+            () -> new ArmorItem(ModArmorMaterials.OBSIDIAN, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> OBSIDIAN_CHESTPLATE = ITEMS.register("obsidian_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.OBSIDIAN, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> OBSIDIAN_LEGGINGS = ITEMS.register("obsidian_leggings",
+            () -> new ArmorItem(ModArmorMaterials.OBSIDIAN, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+    public static final RegistryObject<Item> OBSIDIAN_BOOTS = ITEMS.register("obsidian_boots",
+            () -> new ArmorItem(ModArmorMaterials.OBSIDIAN, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModTab.MTOOLS_TAB)));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
